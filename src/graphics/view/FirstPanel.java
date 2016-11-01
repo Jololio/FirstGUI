ee673ff456d329a1867f57bb2ac0f356285733b6
Jololio <jala9132@740s-pl-120.local>
2016-11-01 12:51:41 -0600

    Added Buttons

--- src/graphics/view/FirstPanel.java
+++ src/graphics/view/FirstPanel.java
@@ -1,17 +1,23 @@
 package graphics.view;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import java.awt.Color;
 import graphics.controller.GraphicsController;
 
 public class FirstPanel extends JPanel
 {
 	private GraphicsController baseController;
+	private JButton colorButton;
+	private JButton randomButton;
 	
 	public FirstPanel(GraphicsController baseController)
 	{
 		super();
 		this.baseController = baseController;
 		
+		this.colorButton = new JButton("Niiiiice Tryyyy");
+		this.randomButton = new JButton("ez");
 		setupPanel();
 		setupLayout();
 		setupListeners();
@@ -19,7 +25,9 @@ public class FirstPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		
+		this.setBackground(Color.ORANGE);
+		this.add(colorButton);
+		this.add(randomButton);
 	}
 	
 	private void setupLayout()
@@ -31,4 +39,6 @@ public class FirstPanel extends JPanel
 	{
 		
 	}
+
+
 }
--- src/graphics/view/GraphicViewer.java
+++ src/graphics/view/GraphicViewer.java
@@ -8,17 +8,21 @@ public class GraphicViewer extends JFrame
 {
 	
 	private GraphicsController baseController;
+	private FirstPanel appPanel;
 	
 	public GraphicViewer(GraphicsController baseController)
 	{
 		super();
 		this.baseController = baseController;
+		this.appPanel = new FirstPanel(baseController);
 		
 		this.setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);
+		this.setTitle("Got 'em Gaming!");
 		this.setSize(new Dimension(600, 400));
 		this.setVisible(true);
 	}
